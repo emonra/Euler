43e3aac63ac29aeb5272f9bfbf3d553b114d0a6a
emonra <41332979+emonra@users.noreply.github.com>
2020-03-20 21:07:14 +0700

    gitignore sandbox

--- Sandbox.py
+++ Sandbox.py
@@ -1,8 +1,2 @@
-from timeit import default_timer as time
-
-start = time()
-
-list_of_multiples = [x for x in range(1,1000) if (x%3==0 or x%5==0)]
-sum2 = sum(list_of_multiples)
-print(sum2)
-print(time()-start)
+while True:
+	print("no")
