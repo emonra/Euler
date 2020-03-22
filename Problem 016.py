2a18e859b0a151cea141a054ee8605c87b6a782d
emonra <emonra2001@gmail.com>
2020-03-22 19:20:48 +0700

    turn into function when available

--- Problem 016.py
+++ Problem 016.py
@@ -2,9 +2,11 @@ from timeit import default_timer as time
 
 start = time()
 number = pow(2,1000)
-Sum = 0
-for i in range(len(str(number))):
-	Sum += int(str(number)[i])
+def DigitSum(number):
+	Sum = 0
+	for i in range(len(str(number))):
+		Sum += int(str(number)[i])
+	return Sum
 
-print(Sum)
+print(DigitSum(number))
 print(time() - start)
