de6c605b4ff1d6a95d696f64da6df2c7680701d3
emonra <emonra2001@gmail.com>
2020-03-22 19:22:24 +0700

    turn ???.py into Problem ???.py

--- Sandbox.py
+++ Sandbox.py
@@ -1,15 +1,20 @@
 #rename to three digits
 import os
-for index in range(100):
-	if index < 10:
-		OldName = os.path.join(os.getcwd(), "Problem " + "0" + str(index) + ".py")
-	else:
-		OldName = os.path.join(os.getcwd(), "Problem " + str(index) + ".py")
-	print(OldName)
-	if os.path.exists(OldName):
+import time
+while True:
+	for index in range(1000):
 		if index < 10:
-			NewName = os.path.join(os.getcwd(), "Problem " + "00" + str(index) + ".py")
+			OldName = os.path.join(os.getcwd(), "00" + str(index) +".py")
+		elif index <100:
+			OldName = os.path.join(os.getcwd(), "0" + str(index) + ".py")
 		else:
-			NewName = os.path.join(os.getcwd(), "Problem " + "0" + str(index) + ".py")
-		os.rename(OldName, NewName)
-print("done")
+			OldName = os.path.join(os.getcwd(), str(index) + ".py")
+		if os.path.exists(OldName):
+			if index < 10:
+				NewName = os.path.join(os.getcwd(), "Problem " + "00" + str(index) + ".py")
+			elif index < 100:
+				NewName = os.path.join(os.getcwd(), "Problem " + "0" + str(index) + ".py")
+			else:
+				NewName = os.path.join(os.getcwd(), "Problem " + str(index) + ".py")
+			os.rename(OldName, NewName)
+	time.sleep(5)
