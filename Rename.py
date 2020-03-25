c497d6f3f4e3b0faa5ee92542ca279b83e9443ca
emonra <41332979+emonra@users.noreply.github.com>
2020-03-25 21:57:28 +0700

    Fix rename to work with numbers under 100

--- Rename.py
+++ Rename.py
@@ -3,12 +3,7 @@
 import os
 
 for index in range(1000):
-	if index < 10:
-		OldName = os.path.join(os.getcwd(), "00" + str(index) +".py")
-	elif index <100:
-		OldName = os.path.join(os.getcwd(), "0" + str(index) + ".py")
-	else:
-		OldName = os.path.join(os.getcwd(), str(index) + ".py")
+	OldName = os.path.join(os.getcwd(), str(index) + ".py")
 	if os.path.exists(OldName):
 		if index < 10:
 			NewName = os.path.join(os.getcwd(), "Problem " + "00" + str(index) + ".py")
