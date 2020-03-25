4f7c5fb48d578b9f76860e306bf12aded2c9c48d
emonra <41332979+emonra@users.noreply.github.com>
2020-03-25 22:24:04 +0700

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,15 +1,16 @@
 #rename to three digits
 #eg. "123.py" will be renamed to "Problem 123.py"
 import os
-
-for index in range(1000):
-	OldName = os.path.join(os.getcwd(), str(index) + ".py")
-	if os.path.exists(OldName):
-		if index < 10:
-			NewName = os.path.join(os.getcwd(), "Problem " + "00" + str(index) + ".py")
-		elif index < 100:
-			NewName = os.path.join(os.getcwd(), "Problem " + "0" + str(index) + ".py")
-		else:
-			NewName = os.path.join(os.getcwd(), "Problem " + str(index) + ".py")
-		os.rename(OldName, NewName)
-exit(0)
+from time import sleep
+while True:
+	for index in range(1000):
+		OldName = os.path.join(os.getcwd(), str(index) + ".py")
+		if os.path.exists(OldName):
+			if index < 10:
+				NewName = os.path.join(os.getcwd(), "Problem " + "00" + str(index) + ".py")
+			elif index < 100:
+				NewName = os.path.join(os.getcwd(), "Problem " + "0" + str(index) + ".py")
+			else:
+				NewName = os.path.join(os.getcwd(), "Problem " + str(index) + ".py")
+			os.rename(OldName, NewName)
+	sleep(30)
