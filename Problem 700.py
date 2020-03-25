6dd2af6a2210d3b9dc9c4f02ea2359d8ce12027b
emonra <41332979+emonra@users.noreply.github.com>
2020-03-25 21:56:59 +0700

    cleanup (incomplete)

--- Problem 700.py
+++ Problem 700.py
@@ -2,15 +2,15 @@ from timeit import default_timer as time
 from math import gcd
 start = time()
 n = 0
-Min = 1117 + 1
+Min = 4503599627370517
 Sum = 0
-while True:
+while (time() - start) < 10:
 	n += 1
-	#temp = (1504170715041707 * n) % 4503599627370517
-	temp = (513 * n) % 1119
+	temp = (1504170715041707 * n) % 4503599627370517
 	if temp < Min:
 		Min = temp
 		Sum += temp
 		print(n, time() - start, temp, Sum)
 	if time() - start > 20:
 		break
+
