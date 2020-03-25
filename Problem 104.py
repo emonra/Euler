17e55dc70ba2ded6cc932436a5cb07095259fa75
emonra <41332979+emonra@users.noreply.github.com>
2020-03-25 21:57:46 +0700

    change comment

--- Problem 104.py
+++ Problem 104.py
@@ -22,7 +22,7 @@ while True:
 	Fib[1] = Fib[2]
 	Fib[2] = Fib[0] + Fib[1]
 	"""checking pandigital-ness makes the script runs for more 
-	than an hour, which means it's probably not efficient"""
+	than two and a half hours, which means it's not efficient"""
 	if count % 10000 == 0:
 		print(count, time() - start)
 	if EndsArePandigital(Fib[2]):
